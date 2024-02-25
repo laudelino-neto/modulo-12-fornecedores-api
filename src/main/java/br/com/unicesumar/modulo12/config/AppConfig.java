8f16589784aa02c8e7636df9e351764161ade498
laudelino-neto <laudelino_sc@hotmail.com>
2024-02-25 16:55:04 -0300

    Finalizada a preparação do projeto para o desenvolvimento em aula

--- src/main/java/br/com/unicesumar/modulo12/config/AppConfig.java
+++ src/main/java/br/com/unicesumar/modulo12/config/AppConfig.java
@@ -1,17 +1,10 @@
 package br.com.unicesumar.modulo12.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import br.com.unicesumar.modulo12.adapters.outbound.data.FornecedorCrudAdapter;
-import br.com.unicesumar.modulo12.application.core.service.FornecedorService;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public FornecedorService fornecedorService(FornecedorCrudAdapter fornecedorCrudAdapter) {
-		return new FornecedorService(fornecedorCrudAdapter);
-	}
+	
+	
 	
 }
